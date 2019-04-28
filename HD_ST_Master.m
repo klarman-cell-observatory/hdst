5603c939e0a6bb130202df5231efa931234f305b
DenisSch <DenisSch@users.noreply.github.com>
2019-04-28 14:57:24 -0400

    Add correction y and run analysis on BC as well

--- HD_ST_Master.m
+++ HD_ST_Master.m
@@ -7,12 +7,23 @@ clear all
 clc
 
 %% Load tsv spot table
-st_spot_table = readtable('CN13_D2_filtered_red_ut.csv','Delimiter','\t');
+%st_spot_table = readtable('CN13_D2_filtered_red_ut.csv','Delimiter','\t');
+st_spot_table = readtable('CN21_E2_filtered_red_ut.csv','Delimiter','\t');
 
 %% Load mask
-st_sc_mask = imread('CN13_D2_HE_Probabilities_mask.tiff');
+%st_sc_mask = imread('CN13_D2_HE_Probabilities_mask.tiff');
+st_sc_mask = imread('CN21_E2_HE_Probabilities_mask.tiff');
+
+% Extract x and y from regionprops
+mask_centroid = regionprops(st_sc_mask,'centroid');
+for i=1:length(mask_centroid)
+    x_centroid(i) = mask_centroid(i).Centroid(1)';
+    y_centroid(i) = mask_centroid(i).Centroid(2)';
+end
 
 %% Extract all CellID's for the spot location
+% Flip centroid to align with spots
+y_centroid_corr=-(y_centroid-min(y_centroid))+(max(y_centroid));
 
 % Extract unique values
 [unique_spots,unique_value_location,ic] = unique(st_spot_table.bc,'stable');
@@ -25,11 +36,18 @@ unique_bc = st_spot_table.bc(unique_value_location);
 % Extract the overlap with the mask
 for i=1:size(unique_spots,1)
     unique_bc{i,2} = st_sc_mask(unique_y(i,1),unique_x(i,1));
+    if unique_bc{i,2} == 0
+        continue
+    else
+    unique_bc{i,3} = x_centroid(1,unique_bc{i,2});
+    unique_bc{i,4} = y_centroid_corr(1,unique_bc{i,2});
+    end
 end
 
-export_table = cell2table(unique_bc,'VariableNames',{'bc','cell_id'});
+export_table = cell2table(unique_bc,'VariableNames',...
+    {'bc','cell_id','x_centroid','y_centroid'});
 
-writetable(export_table,'CellID_Spot_Position.csv');
+writetable(export_table,'CellID_Spot_Position_CN21_E2_filtered_red_ut.csv');
 
 end
 
